2378eeab29610f87727d9c27d5c1af225c3e6c3d
kumarb97 <kumarbamankar@gmail.com>
2022-03-10 13:27:25 +0530

    (Comments Added) First use case of valid first name added

--- UserRegistration.java
+++ UserRegistration.java
@@ -1,3 +1,6 @@
+/*
+ * USER REGISTRATION
+ */
 package userregistration;
 
 import java.util.Scanner;
@@ -7,6 +10,11 @@ import java.util.regex.Matcher;
 public class UserRegistration {
 	public static Scanner sc = new Scanner(System.in);
 	
+	/*
+	 * Method validFirstName is used to take input from user and
+	 * check whether its matches with the given condition/criteria of valid name
+	 * 
+	 */
 	public static void validFirstName() {
 		
 		System.out.println("Enter First Name");
@@ -22,7 +30,4 @@ public class UserRegistration {
 			System.out.println("It is not a valid name, Please enter valid name");
 		}
 	}
-	
-	
-
 }
--- UserRegistrationMain.java
+++ UserRegistrationMain.java
@@ -1,17 +1,43 @@
+/**
+ *@PROBLEM_STATEMENT : UC1
+ * As a User need to enter a valid First Name
+ * First name starts with Cap and has minimum 3 character
+ * @author Kumar Bamankar
+ *
+ */
 package userregistration;
+
 import java.util.Scanner;
 public class UserRegistrationMain {
 	static Scanner sc = new Scanner(System.in);
 	
-	static void display() {
+	/*
+	 * Method display is used to to display welcome message and to take choice
+	 * from user what he/she wants to do.
+	 * In this use case only valid first name option is available.
+	 * 
+	 */
+	public static void display() {
 		System.out.println("_________________________");
 		System.out.println("USER REGISTRATION");
 		System.out.println("_________________________");
 		System.out.println("\nEnter 1 : To valid first name");
 		
 	}
+	
+	/* 
+     * Main method for manipulation linkedList
+     * @param args - Default Java param (Not used)
+	 */
 
 	public static void main(String[] args) {
+		
+	/*
+	 * PROCEDURE :
+	 * 1.Creating a method vaidFirstName to validate first name.
+	 * 2.Creation a method display to display welcome message and take choice from the user.
+	 * 3.Using switch case to implement user's choice.	
+	 */
 		
 		display();
 		int option = sc.nextInt();
